d0a8037f8be62253c134ca3d3da6f8db4f96e33e
Calvin <Calvin.lacc@gmail.com>
2016-07-13 14:03:27 -0600

    Working through implementing the searcher for each HTML line

---
+++ warmup2.rb
@@ -0,0 +1,75 @@
+Node=Struct.new(:tag,:text,:class,:id,:children,:parent)
+
+class Reader 
+  def initialize
+      @document=File.readlines('warmuphtml.html')
+      @pointer
+  end
+
+  def build_tree
+      @root=Node.new([],[],[],[],[],[])
+      @pointer=@root
+      @num_nodes=1
+
+      @document.each do |line|
+        create_nodes(line)
+     end
+     puts @num_nodes
+  end
+
+  def create_nodes(line)
+  	@num_nodes +=1
+      test_node = parse_line(line)
+      if line.include?('<') && !line.include?('</')
+          @pointer.children<< Node.new(test_node.tag,test_node.text,test_node.class,test_node.id,[],@pointer)
+          @pointer=@pointer.children[-1]
+      end
+
+      if line.include?('/<')
+          @pointer=@pointer.parent
+      end
+
+      if !line.include?('<')
+      	@pointer.text=test_node.text
+      end
+  end
+
+  def parse_line(line)
+  	  test_node=Node.new(nil,nil,nil,nil,nil,nil)
+
+  	  if line.include?('<')
+  	  	matching_tag = line.match (/[<](.+?)[ ]/)
+  	  	test_node.tag = matching_tag
+  	  end
+
+	  if line.include?('class=')
+		matching_class = line.match (/class=['"](.+?)['"]/)
+		test_node.class = matching_class
+	  end
+
+	  if line.include?('id=')
+		matching_id = line.match (/id=['"](.+?)['"]/)
+		test_node.id = matching_id
+	  end
+
+	  if !line.include?('<')
+		test_node.text = line
+	  end
+
+	  return test_node
+
+   end
+
+   def render
+   	puts build_tree
+   end
+
+
+
+end
+
+
+
+c = Reader.new 
+c.build_tree
+c.render
