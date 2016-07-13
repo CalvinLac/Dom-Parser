4af6d6396643c393b2d2d0455bfeebb613fec32c
Calvin <Calvin.lacc@gmail.com>
2016-07-12 21:45:46 -0600

    Debugged the parser file

--- warmup1.rb
+++ warmup1.rb
@@ -4,30 +4,34 @@ Attributes = Struct.new(:classes, :id, :name)
 class Parser
 
 	def initialize
-		@html_array = File.readlines('warmuphtml.html')
-		@class = []
-		@id = []
-		@html_array.each do |line|
+		@final_array = []
+		html_array = File.readlines('warmuphtml.html')
+		html_array.each do |line|
 			exact_match(line)
 		end
 	end
 
 
 	def exact_match (tag)
-		attributes = Attributes.new
+		if tag.include?('class=') || tag.include?('id=')
+			attributes = Attributes.new
+		end
 
-		#for class 
+		#for class  
 		if tag.include?('class=')
 			matching_class1 = tag.match (/class=['"](.+?)['"]/)
-			attributes.classes << matching_class1.captures[0].split(" ")
+			attributes.classes = matching_class1.captures[0].split(" ")
 		end
 
 		#for id
 		if tag.include?('id=')
 			matching_class2 = tag.match (/id=['"](.+?)['"]/)
-			@attributes.id << matching_class2.captures[0].split(" ")
+			attributes.id = matching_class2.captures[0].split(" ")
 		end
 
+		if tag.include?('class=') || tag.include?('id=')
+			@final_array << attributes
+		end
 
 	end
 
